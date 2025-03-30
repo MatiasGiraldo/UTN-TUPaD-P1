6cdd426f1a590734099d04df2392b50c3330e83c
MatiasGiraldo <matiasgiraldo1995@gmail.com>
2025-03-30 20:37:04 -0300

    Update MatiasGiraldo-TP2

---
+++ 02 Trabajo Colaborativo/MI-APELLIDO_TP-02.py
@@ -0,0 +1,33 @@
+Práctico 2: Git y GitHub
+Actividades: 
+1)	Contestar las siguientes preguntas utilizando las guías y documentación proporcionada (Desarrollar las respuestas)
+
+a-	GitHub es una plataforma para guardar todos los proyectos, compartirlos y trabajar en conjunto con otras personas el código. 
+b-	Para crear un repositorio en GitHub lo podes hacer desde la página (le das nombre, lo pones enm publico o privado,etc) y en tu terminal (ubicándote en una carpeta que sirva de repositorio) colocar “git init”.
+c-	Para crear una rema en Git solo usas el comando “git branch (nombre de la rama)”.
+d-	Para cambiar a una rama en Git se utiliza el comando “git checkout (nombre de la rama a la que queremos cambiar)”.
+e-	Para fusionar ramas en Git se utiliza el comando “git merge (rama)”, anteriormente te tenés que ubicar con checkout para que se fusionen en donde estas ubicado.
+f-	Para crear un commit en Git es simple, “git commit -m “palabra o frase reservada para el cambio””.
+g-	Para enviar un commit a Github tiene que vincularse el repositorio local con el subido en Github, se tendría que hacer un push para subirlo.
+h-	Un repositorio remoto es el que se encuentra en la red, compartido para los demás si es público.
+i-	Para subir un repositorio remoto a Git necesitas vincularlo con Github a través de un link y comandos.
+j-	Para empujar cambios a un repositorio remoto necesitas registrar los cambios “git add .”, luego git commit -m “nombre del cambio” y luego hacer un push de la rama en la que estás para que se guarde ahí.
+k-	Para tirar cambios de un repositorio necesitas usar el comando “git pull” y un link del repositorio remoto.
+l-	Un fork de repositorio es una copia de un repositorio de internet.
+m-	Para crear un fork de un repositorio solo lo buscas y si es publico tenes acceso, sino necesitas permiso del dueño del repositorio. 
+n-	Para enviar una solicitud de extracción (pull request) a un repositorio podes clonar el repositorio, trabajar en él y colaborar con el dueño del repositorio para que suba los cambios a través de Compare & pull request.
+o-	Para aceptar una solicitud de extracción vas a Pull Requests, revisas los cambios y si está bien haces un merge del pull request y confirmas el merge.
+p-	Una etiqueta en Git es un marcador que se usa para señalar puntos específicos del historial, normalmente se usan para versiones del código.
+q-	Para crear una etiqueta podes usar el comando git tag “nombre de etiqueta”.
+r-	Para enviar un etiqueta necesitas utilizar el comando  “git push origin [etiqueta]”.
+s-	Un historial de Git es la secuencia de commits que muestran los cambios en el proyecto.
+t-	Para ver el historial de Git usas el comando “git log”.
+u-	Para buscar en el historial de Git podes usar git log –grep=”alguna palabra clave” . Para borrar el historial de Git
+v-	Un repositorio privado en GitHub es uno que solo puede darte permiso su creador para acceder a él.
+w-	Un repositorio público en GitHub es aquel que puede acceder cualquier persona.
+x-	Para crear un repositorio público en GitHub solo tienes que marcar la opción para que lo sea.
+y-	Para compartir un repositorio público en GitHub solo necesitas la URL del repositorio.
+
+2)	https://github.com/MatiasGiraldo/Repositorio
+
+3)	https://github.com/MatiasGiraldo/Repositorio-TP2-Act3
